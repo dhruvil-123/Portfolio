1df0b12b5a76034caa87ebd23f5cbe6ce4e8ec8a
Dhruvil Dhanani <102502128+dhruvil-123@users.noreply.github.com>
2024-05-27 12:57:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { useState } from "react";
 import { darkTheme, lightTheme } from './utils/Themes.js'
 import Navbar from "./components/Navbar";
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
 import HeroSection from "./components/HeroSection";
 // import About from "./components/About";
 import Skills from "./components/Skills";
